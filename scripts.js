5340d559383dcfa1a3f2e8c2695c57e8f4d2b494
Whitney Badge <whitney.badge@gmail.com>
2018-05-30 23:53:04 -0400

    styling

--- scripts.js
+++ scripts.js
@@ -1,30 +1,30 @@
-$("body").mouseover(function(e) {
+// $("body").mouseover(function(e) {
 
-  e = e || window.event; var cursor = { y: 0 }; cursor.y = e.pageY; //Cursor YPos
-  var papaWindow = parent.window;
-  var $pxFromTop = $(papaWindow).scrollTop();
-  var $userScreenHeight = $(papaWindow).height();
+//   e = e || window.event; var cursor = { y: 0 }; cursor.y = e.pageY; //Cursor YPos
+//   var papaWindow = parent.window;
+//   var $pxFromTop = $(papaWindow).scrollTop();
+//   var $userScreenHeight = $(papaWindow).height();
 
-  if (cursor.y > (($userScreenHeight + $pxFromTop) / 1.25)) {
+//   if (cursor.y > (($userScreenHeight + $pxFromTop) / 1.25)) {
 
-         if ($pxFromTop < ($userScreenHeight * 3.2)) {
+//          if ($pxFromTop < ($userScreenHeight * 3.2)) {
 
-                   papaWindow.scrollBy(0, ($userScreenHeight / 30));
-             }
-        }
-  else if (cursor.y < (($userScreenHeight + $pxFromTop) * .75)) {
+//                    papaWindow.scrollBy(0, ($userScreenHeight / 30));
+//              }
+//         }
+//   else if (cursor.y < (($userScreenHeight + $pxFromTop) * .75)) {
 
-        papaWindow.scrollBy(0, -($userScreenHeight / 30));
+//         papaWindow.scrollBy(0, -($userScreenHeight / 30));
 
-        }
+//         }
 
-   }); //End mouseover()
+//    }); //End mouseover()
 
-var typetest = new Typed('#typetest', {
-  strings: ['Mother of Pearl','Witch of the Wave','Cape of Good Hope','Stone Fleet'],
-    typeSpeed: 60,
-    backSpeed: 60,
-    attr: 'placeholder',
-    bindInputFocusEvents: true,
-    loop: true
-});
+// var typetest = new Typed('#typetest', {
+//   strings: ['Mother of Pearl','Witch of the Wave','Cape of Good Hope','Stone Fleet'],
+//     typeSpeed: 60,
+//     backSpeed: 60,
+//     attr: 'placeholder',
+//     bindInputFocusEvents: true,
+//     loop: true
+// });
